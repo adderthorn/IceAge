ccf4ed637f08cd7795029c166e6631b7ebfa123c
Noah Wood <noah@noahw.org>
2023-08-05 10:53:15 -0400

    Working changes. This is all WIP.

--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -12,6 +12,11 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using IceAge.Interconnect;
+using System.Net.Http;
+using Mastonet;
+using System.Diagnostics;
+using Mastonet.Entities;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -22,13 +27,40 @@ namespace IceAge;
 /// </summary>
 public sealed partial class MainWindow : Window
 {
+    private const string INSTANCE = "XXXXX";
+
+    public HttpClient HttpClient { get; set; }
+    public MastodonClient MastodonClient { get; set; }
+    public AppRegistration AppRegistration { get; set; }
+    public AuthenticationClient AuthenticationClient { get; set; }
+    public Auth Auth { get; set; }
+
     public MainWindow()
     {
         this.InitializeComponent();
+        this.HttpClient = new HttpClient();
+        AuthenticationClient = new AuthenticationClient(INSTANCE, HttpClient);
     }
 
-    private void myButton_Click(object sender, RoutedEventArgs e)
+    private async void myButton_Click(object sender, RoutedEventArgs e)
     {
-        myButton.Content = "Clicked";
+        AppRegistration ??= await AuthenticationClient.CreateApp("IceAge", Scope.Read);
+        if (string.IsNullOrWhiteSpace(AuthCodeBox.Text))
+        {
+            myButton.Content = "Getting Auth Code...";
+            var url = AuthenticationClient.OAuthUrl();
+            await Windows.System.Launcher.LaunchUriAsync(new Uri(url));
+            myButton.Content = "Get Timeline";
+            return;
+        }
+        myButton.Content = "Getting Toots...";
+        Auth ??= await AuthenticationClient.ConnectWithCode(AuthCodeBox.Text);
+        MastodonClient ??= new MastodonClient(INSTANCE, Auth.AccessToken, HttpClient);
+        var timeline = await MastodonClient.GetHomeTimeline();
+        foreach (var item in timeline)
+        {
+            Debug.WriteLine(item);
+        }
+        myButton.Content = "Refresh";
     }
 }
