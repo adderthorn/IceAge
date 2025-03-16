976594750c7ab955ba7c087f0fa0cca708513d07
Noah Wood <noah@noahw.org>
2025-03-16 15:53:19 -0400

    WIP - Refactored into TootViewModel

--- src/ViewModels/TootViewModel.cs
+++ src/ViewModels/TootViewModel.cs
@@ -19,6 +19,7 @@ namespace IceAge.ViewModels;
 public partial class TootViewModel : ObservableObject
 {
     private readonly MastodonClient _client;
+    private readonly DispatcherTimer _timer;
 
     [ObservableProperty]
     public partial Status Status { get; set; }
@@ -73,9 +74,62 @@ public partial class TootViewModel : ObservableObject
     [ObservableProperty]
     public partial ObservableCollection<UserControl> AttachmentControls { get; set; }
 
-    public TootViewModel(Status status, MastodonClient client, bool shortenHyperlinks)
+    public string CreatedTimeAgo
     {
-        _client = client;
+        get
+        {
+            var diff = DateTime.Now - Created;
+            if (diff.TotalDays > 365)
+            {
+                return $"{(int)(diff.TotalDays / 365)}y";
+            }
+            else if (diff.TotalDays > 30)
+            {
+                return $"{(int)(diff.TotalDays / 30)}m";
+            }
+            else if (diff.TotalDays > 1)
+            {
+                return $"{(int)diff.TotalDays}d";
+            }
+            else if (diff.TotalHours > 1)
+            {
+                return $"{(int)diff.TotalHours}h";
+            }
+            else if (diff.TotalMinutes > 1)
+            {
+                return $"{(int)diff.TotalMinutes}m";
+            }
+            else
+            {
+                if (diff.TotalSeconds < 0)
+                {
+                    return "0s";
+                }
+                return $"{(int)Math.Ceiling(diff.TotalSeconds)}s";
+            }
+        }
+    }
+
+    public string StatusGlyph => LockedAccount
+        ? "\xE785" //Unlock
+        : "\xE774"; //Globe
+
+    public TootViewModel(MastodonInterop mastodonInterop, Status status)
+        //(Status status, MastodonClient client, bool shortenHyperlinks)
+    {
+        _client = mastodonInterop.MastodonClient;
+        _timer = new DispatcherTimer();
+        _timer.Interval = TimeSpan.FromSeconds(30);
+        _timer.Tick += (s, e) =>
+        {
+            if (Created.AddHours(2) <= DateTime.Now)
+            {
+                _timer.Stop();
+            }
+            OnPropertyChanged(nameof(Created));
+            OnPropertyChanged(nameof(CreatedTimeAgo));
+        };
+        AttachmentControls = new ObservableCollection<UserControl>();
         Status = status;
     }
 
@@ -84,8 +138,22 @@ public partial class TootViewModel : ObservableObject
         OnPropertyChanged(nameof(BoostedWeight));
     }
 
+    partial void OnCreatedChanged(DateTime value)
+    {
+        OnPropertyChanged(nameof(CreatedTimeAgo));
+    }
+
+    partial void OnLockedAccountChanged(bool value)
+    {
+        OnPropertyChanged(nameof(StatusGlyph));
+    }
+
     partial void OnStatusChanged(Status value)
     {
+        if (_timer.IsEnabled)
+        {
+            _timer.Stop();
+        }
         IsFavorite = value.Favourited ?? false;
         IsBoosted = value.Reblogged ?? false;
         IsBotAccount = value.Account.Bot ?? false;
@@ -141,6 +209,39 @@ public partial class TootViewModel : ObservableObject
                     throw new NotImplementedException();
                     break;
             }
+        }
+        _timer.Start();
+    }
+
+    public void ToggleFavorite(bool? isFavorite = null)
+    {
+        isFavorite ??= !IsFavorite;
+        IsFavorite = isFavorite.Value;
+        if (IsFavorite)
+        {
+            FavoriteCount++;
+            _client.Favourite(Status.Id);
+        }
+        else
+        {
+            FavoriteCount--;
+            _client.Unfavourite(Status.Id);
+        }
+    }
+
+    public void ToggleBoost(bool? isBoost = null)
+    {
+        isBoost ??= !IsBoosted;
+        IsBoosted = isBoost.Value;
+        if (IsBoosted)
+        {
+            BoostedCount++;
+            _client.Reblog(Status.Id);
+        }
+        else
+        {
+            BoostedCount--;
+            _client.Unreblog(Status.Id);
         }
     }
 }
