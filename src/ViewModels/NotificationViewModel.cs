652fc642866280cfd0a68830e87d372721a49e45
Noah Wood <noah@noahw.org>
2025-06-28 12:44:04 -0400

    Adds glyphs and text to notifications

--- src/ViewModels/NotificationViewModel.cs
+++ src/ViewModels/NotificationViewModel.cs
@@ -6,11 +6,15 @@ using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using IceAge.Interop;
 using Mastonet.Entities;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Documents;
+using Microsoft.Windows.ApplicationModel.Resources;
 
 namespace IceAge.ViewModels;
 public partial class NotificationViewModel : ObservableObject
 {
     private readonly MastodonInterop _interop;
+    private readonly ResourceLoader _resourceLoader;
 
     public TootViewModel TootViewModel { get; private set; }
 
@@ -23,9 +27,34 @@ public partial class NotificationViewModel : ObservableObject
     [ObservableProperty]
     public partial string Type { get; set; }
 
+    public string Phrase => _resourceLoader.GetString($"NotificationType/{this.Type}");
+
+    public String Glyph
+    {
+        get
+        {
+            switch (this.Type)
+            {
+                case "mention":
+                    return "\xE97A";
+                case "reblog":
+                    return "\xE8EB";
+                case "follow":
+                    return "\xE8FA";
+                case "favourite":
+                    return "\xE734";
+                case "poll":
+                    return "\xEADF";
+                default:
+                    return string.Empty;
+            }
+        }
+    }
+
     public NotificationViewModel(MastodonInterop mastodonInterop, Notification notification)
     {
-        _interop = mastodonInterop;
+        this._interop = mastodonInterop;
+        this._resourceLoader = new ResourceLoader();
         Notification = notification;
     }
 
